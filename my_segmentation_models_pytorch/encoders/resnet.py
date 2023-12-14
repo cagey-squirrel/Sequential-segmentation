05235012a0e6a7c9d6981ed74d21ae7b67b52001
cagey-squirrel <cvetkovicaleksandar1302@gmail.com>
2023-12-14 02:02:35 +0100

    NOT READY YET mutual attention resnet

--- my_segmentation_models_pytorch/encoders/resnet.py
+++ my_segmentation_models_pytorch/encoders/resnet.py
@@ -70,6 +70,47 @@ class ResNetEncoder(ResNet, EncoderMixin):
         super().load_state_dict(state_dict, **kwargs)
 
 
+class MAResNetEncoder(ResNet, EncoderMixin):
+    def __init__(self, out_channels, depth=5, **kwargs):
+        super().__init__(**kwargs)
+        self._depth = depth
+        self._out_channels = out_channels
+        self._in_channels = 3
+
+
+
+        del self.fc
+        del self.avgpool
+
+    def get_stages(self):
+        return [
+            nn.Identity(),
+            nn.Sequential(self.conv1, self.bn1, self.relu),
+            nn.Sequential(self.maxpool, self.layer1),
+            self.layer2,
+            self.layer3,
+            self.layer4,
+        ]
+
+    def forward(self, x):
+        stages = self.get_stages()
+
+        features = []
+        for i in range(self._depth + 1):
+            x = stages[i](x)
+            print(x.shape)
+            features.append(x)
+        
+        exit(-1)
+
+        return features
+
+    def load_state_dict(self, state_dict, **kwargs):
+        state_dict.pop("fc.bias", None)
+        state_dict.pop("fc.weight", None)
+        super().load_state_dict(state_dict, **kwargs)
+
+
 new_settings = {
     "resnet18": {
         "ssl": "https://dl.fbaipublicfiles.com/semiweaksupervision/model_files/semi_supervised_resnet18-d92f0530.pth",  # noqa
@@ -148,6 +189,15 @@ resnet_encoders = {
         "params": {
             "out_channels": (3, 64, 64, 128, 256, 512),
             "block": SCSEBlock,
+            "layers": [3, 4, 6, 3],
+        },
+    },
+    "resnet34-ma": {
+        "encoder": MAResNetEncoder,
+        "pretrained_settings": pretrained_settings["resnet34"],
+        "params": {
+            "out_channels": (3, 64, 64, 128, 256, 512),
+            "block": BasicBlock,
             "layers": [3, 4, 6, 3],
         },
     },
