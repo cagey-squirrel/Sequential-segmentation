18ad8534a6623cfa4d55a5a738f3fdba8b9cd00e
cagey-squirrel <cvetkovicaleksandar1302@gmail.com>
2023-12-27 12:58:39 +0100

    Adding Layer Normalization option for Unet

--- src/models/smp_unet.py
+++ src/models/smp_unet.py
@@ -6,9 +6,9 @@ import my_segmentation_models_pytorch as smp
 class SmpUnet(torch.nn.Module):
 
 
-    def __init__(self, encoder, in_channels, out_channels, weights, attention=None, device=None):
+    def __init__(self, encoder, in_channels, out_channels, weights, attention=None, layer_normalization_type=None, device=None):
         super().__init__()
-        self.model = smp.Unet(encoder_name=encoder, in_channels=in_channels, classes=out_channels, encoder_weights=weights, activation='sigmoid', decoder_attention_type=attention, device=device)
+        self.model = smp.Unet(encoder_name=encoder, in_channels=in_channels, classes=out_channels, encoder_weights=weights, activation='sigmoid', decoder_use_batchnorm=layer_normalization_type, decoder_attention_type=attention, device=device)
 
 
     def forward(self, images):
